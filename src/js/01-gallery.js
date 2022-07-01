f21dfc253a79dee4b924d6288889747d9f9dec7b
n44bok <vamromanov@gmail.com>
2022-07-01 10:21:09 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItem = ({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" style="display:block"/>
+</a>
+</div>`
+    
+};
+
+const galleryMarkUp = galleryItems.map(galleryItem).join('');
+
+const galleryCollection = document.querySelector('.gallery');
+galleryCollection.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+
+});
